02512dddd4ead6166fa2cc96716c4f3ea53cc989
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2022-12-18 21:03:28 -0300

    tornando classes para privado

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -4,14 +4,14 @@ void main() {
   runApp(const PerguntaApp());
 }
 
-class PerguntasAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntasAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
-    print(perguntaSelecionada);
+    print(_perguntaSelecionada);
   }
 
   @override
@@ -28,17 +28,17 @@ class PerguntasAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Text(perguntas[perguntaSelecionada]),
+            Text(perguntas[_perguntaSelecionada]),
             ElevatedButton(
-              onPressed: responder,
+              onPressed: _responder,
               child: const Text('Resposta 1'),
             ),
             ElevatedButton(
-              onPressed: responder,
+              onPressed: _responder,
               child: const Text('Resposta 2'),
             ),
             ElevatedButton(
-              onPressed: responder,
+              onPressed: _responder,
               child: const Text('Resposta 3'),
             ),
           ],
@@ -52,7 +52,7 @@ class PerguntaApp extends StatefulWidget {
   const PerguntaApp({super.key});
 
   @override
-  PerguntasAppState createState() {
-    return PerguntasAppState();
+  _PerguntasAppState createState() {
+    return _PerguntasAppState();
   }
 }
